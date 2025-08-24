1b953bff5db07c3865a0fab4ad145056e4605884
MrHid3 <gkornas@proton.me>
2025-08-24 14:15:01 +0200

    moved FE from banzai-tournament-BE

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { engine } from 'express-handlebars';
+import "dotenv/config"
+import cors from 'cors';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', cors(), (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/pages/index.html'));
+})
+
+app.get('/dodawanie', cors(), (req, res) => {
+    res.render("addCompetitors", {backendURL: process.env.BACKEND_URL});
+})
+
+app.listen(4000, () => {
+    console.log('http://localhost:4000')
+});
