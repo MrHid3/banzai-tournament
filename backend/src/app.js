da885bdae8506ad42b07295c21ce00dfa37c4b2c
gkornas <gkornas@proton.me>
2025-09-16 08:31:51 +0200

    db.js got integrated into app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,21 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import cors from 'cors';
+import pg from 'pg';
+import dotenv from 'dotenv';
 
-import pool from './server_scripts/db.js';
+dotenv.config("../");
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const pool = new pg.Pool({
+    host: process.env.DB_HOST,
+    port: process.env.POSTGRESDB_DOCKER_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
 const app = express();
 
